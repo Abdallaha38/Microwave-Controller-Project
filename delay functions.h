2327632eb576dfe669a66cafb50f62e35425e019
Bumble <63314839+Kariem816@users.noreply.github.com>
2022-04-28 02:12:36 +0200

    Update delay functions.h

--- delay functions.h
+++ delay functions.h
@@ -1,16 +1,23 @@
 #include "tm4c123gh6pm.h"
-	
+
+// Systick timer initiation
+void systick_Init(){
+   NVIC_ST_CTRL_R = 0;
+	 NVIC_ST_CURRENT_R = 0;
+	 NVIC_ST_CTRL_R = 0x05;
+}
+
 // Function that makes 1 micro seconed delay
 void systick_wait_1us(){
-   NVIC_ST_RELOAD_R = 80 - 1;
+   NVIC_ST_RELOAD_R = 16 - 1;
 	 NVIC_ST_CURRENT_R = 0;
 	 while (( NVIC_ST_CTRL_R & 0x00010000) == 0){}
 }
 
 // Function that makes multiple of micro seconed delay
-void delay_micro( int time){  // the argument is no. of micro-seconds i.e delay(5) it will generate a 5-micro-sec delay
+void delay_us(int time){  // the argument is no. of micro-seconds i.e delay(5) it will generate a 5-micro-sec delay
    int i;
-	 for(i=0;i< time; i++){
+	 for(i = 0; i < time; i++){
 	 systick_wait_1us();
 	 }	 
 }
@@ -18,15 +25,15 @@ void delay_micro( int time){  // the argument is no. of micro-seconds i.e delay(
 
 // Function that makes 1 ms delay
 void systick_wait_1ms(){
-   NVIC_ST_RELOAD_R = 80000 - 1;
+   NVIC_ST_RELOAD_R = 16000 - 1;
 	 NVIC_ST_CURRENT_R = 0;
 	 while (( NVIC_ST_CTRL_R & 0x00010000) == 0){}
 }
 
 // Function that makes multiple of millie seconed delay
-void delay_millie( int time){  // the argument is no. of m-seconds i.e delay(5) it will generate a 5-m-sec delay
+void delay_ms(int time){  // the argument is no. of m-seconds i.e delay(5) it will generate a 5-m-sec delay
    int i;
-	 for(i=0;i< time; i++){
+	 for(i = 0; i < time; i++){
 	 systick_wait_1ms();
 	 }	 
 }
@@ -44,7 +51,7 @@ void delay_s(int time){  // the argument is no. of seconds i.e delay(5) it will 
 void delay_m(int time){    // the argument is no. of min i.e delay(5) it will generate a 5-min delay
    int i;
 	 int delay = time * 60;
-	 for(i = 0 ; i < delay ; i++){
+	 for(i = 0; i < delay; i++){
 	       delay_s(1);
 	 }	 
 }
