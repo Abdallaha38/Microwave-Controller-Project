2c3f9fbffe79595fc0cb4c2ca3a1942c2c533a99
Migz55 <104398281+Migz55@users.noreply.github.com>
2022-04-28 02:13:31 +0200

    Add files via upload
    added systick intialization function and change frequncy to 16 MG Hz

---
+++ delay functions (updated).h
@@ -0,0 +1,57 @@
+#include "tm4c123gh6pm.h"
+
+// Systick timer initiation
+void systick_init(){
+   NVIC_ST_CTRL_R = 0;
+	 NVIC_ST_CURRENT_R = 0;
+	 NVIC_ST_CTRL_R = 0x05;
+}
+
+// Function that makes 1 micro seconed delay
+void systick_wait_1us(){
+   NVIC_ST_RELOAD_R = 16 - 1;    //frequency is 16MGHz
+	 NVIC_ST_CURRENT_R = 0;
+	 while (( NVIC_ST_CTRL_R & 0x00010000) == 0){}
+}
+
+// Function that makes multiple of micro seconed delay
+void delay_us(int time){  // the argument is no. of micro-seconds i.e delay(5) it will generate a 5-micro-sec delay
+   int i;
+	 for(i=0;i< time; i++){
+	 systick_wait_1us();
+	 }	 
+}
+
+
+// Function that makes 1 ms delay
+void systick_wait_1ms(){
+   NVIC_ST_RELOAD_R = 16000 - 1;     //frequency is 16MGHz
+	 NVIC_ST_CURRENT_R = 0;
+	 while (( NVIC_ST_CTRL_R & 0x00010000) == 0){}
+}
+
+// Function that makes multiple of millie seconed delay
+void delay_ms(int time){  // the argument is no. of m-seconds i.e delay(5) it will generate a 5-m-sec delay
+   int i;
+	 for(i=0;i< time; i++){
+	 systick_wait_1ms();
+	 }	 
+}
+
+// Function that makes 1 sec or multiple of it  delay
+void delay_s(int time){  // the argument is no. of seconds i.e delay(5) it will generate a 5-sec delay
+	int i;
+	int delay = time * 1000;
+	 for(i = 0 ; i < delay ; i++){
+	       systick_wait_1ms();
+	 }	 
+}
+
+// Function that makes 1 min or multiple of it  delay
+void delay_m(int time){    // the argument is no. of min i.e delay(5) it will generate a 5-min delay
+   int i;
+	 int delay = time * 60;
+	 for(i = 0 ; i < delay ; i++){
+	       delay_s(1);
+	 }	 
+}
