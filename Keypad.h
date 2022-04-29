dcf09c766a80cb54e23aa35b19fc2c1f778376f7
Abdallaha38 <1901531@eng.asu.edu.eg>
2022-04-29 10:35:46 +0200

    Update Keypad.h

--- Keypad.h
+++ Keypad.h
@@ -26,14 +26,14 @@ void keypad_Init(void)
 	
 }
 
-// Function that makes 1 micro seconed delay
+// Function that makes 1 micro second delay
 void systick_1us(){
    NVIC_ST_RELOAD_R = 16 - 1;    //frequency is 16MGHz
 	 NVIC_ST_CURRENT_R = 0;
 	 while (( NVIC_ST_CTRL_R & 0x00010000) == 0){}
 }
 
-// Function that makes multiple of micro seconed delay
+// Function that makes multiple of micro second delay
 void delay_uus(int time){  // the argument is no. of micro-seconds i.e delay(5) it will generate a 5-micro-sec delay
    int i;
 	 for(i=0;i< time; i++){
@@ -54,17 +54,17 @@ char keypad_clicked()
 {
   while(1)
   {
-    for(short i = 0; i < 4; i++)    //Scan columns 
+	short i, j;
+    for(i = 0; i < 4; i++)    //Scan columns 
     {
-      GPIO_PORTD_DATA_R = (1U << i);
-			delay_uus(5);									//Debounce delay
-
-      for(short j = 0; j < 4; j++)  //Scan rows
+      GPIO_PORTD_DATA_R = (0x01 << i);
+      for(j = 0; j < 4; j++)  //Scan rows
       {
-        if((GPIO_PORTA_DATA_R & 0xF0) == (1U << (j+4)))
+        if((GPIO_PORTA_DATA_R & 0xF0) == (0x10 << j))
           return keys[j][i];
       }
     }
+	delay_uus(10);									//Debounce delay
   }
 }
 
