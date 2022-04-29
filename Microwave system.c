1b4aa2b48631adb95ff66d8299a0e39ad4316e86
Migz55 <104398281+Migz55@users.noreply.github.com>
2022-04-30 01:55:02 +0200

    Add files via upload

---
+++ Microwave system.c
@@ -0,0 +1,75 @@
+#include "tm4c123gh6pm.h"
+#include "string.h"
+#include "stdio.h"
+#include "keypad.h"
+#include "ctype.h"
+
+void choose_meal(){
+	
+ char x ;
+ int i,y,z,o;
+ 
+ 
+ lcm_print("Enter letter(A-B-C-D) to choose a meal");
+ lcm_instruction(clear_display);
+ keypad_clicked(x);
+   if(!isalpha(x)){
+  lcm_print_string("Invalid input !");
+  lcm_instruction(clear_display); 
+}
+switch(x){
+	case 'A':
+		keypad_clicked(x);
+	  lcm_print_string("Popcorn");
+	  print_delay(0,60);
+	   
+	   
+	break;
+	case 'B':
+		keypad_clicked(x);
+	lcm_print_string("Beef weight?");
+	lcm_instruction(clear_display); 
+	keypad_clicked(i);
+	lcm_instruction(clear_display);
+	lcm_print_int(i);
+	delay_s(2);
+	z = 30*i;
+	lcm_print("Time remaining = z seconds");
+	print_delay(0,z);
+	break;
+    case 'C' :
+	lcm_print_string("Chicken weight?");
+		lcm_instruction(clear_display);
+		keypad_clicked(i);
+		lcm_instruction(clear_display);
+		lcm_print_int(i);
+		delay_s(2);
+		z = 12*i;
+	lcm_print("Time remaining = z seconds");
+		print_delay(0,z);
+		
+	break;	
+	case 'D' :
+	lcm_print_string("Cooking Time?");
+	lcm_instruction(clear_display);
+	lcm_print("You must use numbers (1-30) as 00:00");
+	lcm_instruction(clear_display);
+	keypad_clicked(i,y,z,o);
+	lcm_instruction(clear_display);
+	print_delay(o,z ,y,i);
+	lcm_instruction(clear_display);
+	break;
+	
+}
+  
+  }
+
+int main(){
+	
+
+  	do{
+	  
+  	 choose_meal();
+  	}
+	  while(1);	
+	}
