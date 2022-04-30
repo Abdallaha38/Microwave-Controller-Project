7a9ad96adba51ffa7d8db315f14da28bfa519bf7
Bumble <63314839+Kariem816@users.noreply.github.com>
2022-04-30 15:19:14 +0200

    Update Microwave system.c

--- Microwave system.c
+++ Microwave system.c
@@ -1,39 +1,74 @@
 #include "tm4c123gh6pm.h"
 #include "LCM commands.h"
-#include "string.h"
-#include "stdio.h"
 #include "keypad.h"
 #include "ctype.h"
 
+
+
+char t[5] = {0, 0, :, 0, 0};
+int minutes, seconds, i;
+
+void timer(){
+	seconds = 10 * (t[3] - '0') + (t[4] - '0');
+	minutes = 10 * (t[0] - '0') + (t[1] - '0');
+	print_delay(minutes, seconds);
+}
+
+void check_start(char x){
+	if(x == '*')
+		timer();
+	else{
+		t[0] = t[1];
+		t[1] = t[3];
+		t[3] = t[4];
+		t[4] = x;
+	}
+}
+
+void cookingTimer(){
+	lcm_print_string(t);
+	lcm_movecursor(0, 4);
+	for(i = 0; i < 3; i++){
+		check_start(keypad_clicked());
+		lcm_instruction(clear_display);
+		lcm_print_string(t);
+		lcm_movecursor(0, 4);
+		delay_ms(250);
+	}
+	check_start(keypad_clicked());
+	lcm_instruction(clear_display);
+	lcm_print_string(t);
+	while(keypad_clicked() != '*');
+	timer();
+}
+
 void choose_meal(){
 	
-	char x, m;
-	char temp[4];
-	int i, y, z, o;
+	char m;
+	int i, z;
 	char kilos[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	short count;
  
-	lcm_print("Enter letter(A-B-C-D) to choose a meal");
-	lcm_instruction(clear_display);
-	x = keypad_clicked();
-	if(!isalpha(x)){
+	//lcm_print("Enter letter (A-B-C-D) to choose a meal");
+	//lcm_instruction(clear_display);
+	m = keypad_clicked();
+	/*if(!isalpha(x)){
 		lcm_print_string("Invalid input !");
 		lcm_instruction(clear_display); 
-	}
+	}*/
 	
 	switch(x){
 		
 		case 'A':
 			lcm_print_string("Popcorn");
-			print_delay(0, 60); 
+			print_delay(1, 0); 
 			break;
 		
 		case 'B':
-			lcm_print_string("Beef weight?");
-			lcm_instruction(clear_display); 
+			lcm_print_string("Beef weight?"); 
 			m = keypad_clicked();
 			//Checking for valid kilos entered (1-9)
-			for(count = 0; count< 9; count++)
+			for(count = 0; count < 9; count++)
 				{
 					lcm_instruction(clear_display);
 					if(m == kilos[count])
@@ -54,12 +89,11 @@ void choose_meal(){
 			i = m - '0';
 			z = 30 * i;
 			//lcm_print("Time remaining = z seconds");
-			print_delay(0,z);
+			print_delay((z / 60), (z % 60));
 			break;
    
 		case 'C' :
 			lcm_print_string("Chicken weight?");
-			lcm_instruction(clear_display);
 			m = keypad_clicked();
 			//Checking for valid kilos entered (1-9)
 			for(count = 0; count< 9; count++)
@@ -83,12 +117,12 @@ void choose_meal(){
 			i = m - '0';
 			z = 12 * i;
 			//lcm_print("Time remaining = z seconds");
-			print_delay(0,z);
+			print_delay((z / 60), (z % 60));
 			break;	
 		
 		case 'D' :
 			lcm_print_string("Cooking Time?");
-		
+			
 		//Printing time with no conditions neither a way for counting down, yet :)
 			m = keypad_clicked();
 			lcm_instruction(clear_display);
