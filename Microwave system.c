623659b0789cbc215c8ec2ab597559fdedc75248
Migz55 <104398281+Migz55@users.noreply.github.com>
2022-05-05 01:43:14 +0200

    Update Microwave system.c
    Added Comments & descriptions

--- Microwave system.c
+++ Microwave system.c
@@ -3,45 +3,45 @@
 #include "LCM Commands.h"
 #include "interrupts.h"
 
-char t[5] = { '0', '0', ':', '0', '0' };
+char t[5] = { '0', '0', ':', '0', '0' };  //temporary array to store and show remaining time
 int minutes, seconds, i;
 
-void doorLockActive(int x) {
+void doorLockActive(int x) {         //function to activiate door-lock interrupt
 	if (!x)
 		GPIO_PORTD_IM_R &= ~0x80;
 	else if (x)
 		GPIO_PORTD_IM_R |= 0x80;
 }
 
-void pauseActive(int x) {
+void pauseActive(int x) {            //function to (pause door-lock activiation) i.e. interruption
 	if (!x)
 		GPIO_PORTF_IM_R &= ~0x10;
 	else if (x)
 		GPIO_PORTF_IM_R |= 0x10;
 }
 
-void choose_meal() {
+void choose_meal() {       //main function to choose a meal
 
-	char m;
-	int i, z;
-	short count;
+	char m;    //character variable to check letter entered by user
+	int i, z;     //integer variables to hold the time taken by microwave to finish
+	short count;    //variable to be used in checking number of kilos entered by user
 
 	pauseActive(0);
 	doorLockActive(0);
 	lcm_instruction(clear_display);
-	lcm_print("Welcome");
+	lcm_print("WELCOME !!");
 	delay_s(2);
-Begin:
-	lcm_movecursor(0, 0);
-	lcm_print("Choose a program");
-	lcm_movecursor(1, 0);
+Begin:        //start the program
+	lcm_movecursor(0, 0);  //1st line in LCD
+	lcm_print("Choose a meal");
+	lcm_movecursor(1, 0);    //2nd line in LCD
 	lcm_print("from (A-B-C-D)");
-	m = keypad_clicked();
+	m = keypad_clicked();     //waiting for letter entered by user
 	if (m != 'A' && m != 'B' && m != 'C' && m != 'D') {
 		lcm_instruction(clear_display);
-		lcm_print("Err");
+		lcm_print("Err");     //error message if user hasn't choosen a correct letter
 		delay_s(1);
-		goto Begin;
+		goto Begin;         //branch to Begin lable to start from beggining
 	}
 	lcm_instruction(clear_display);
 	pauseActive(1);
@@ -54,22 +54,21 @@ Begin:
 		seconds = 0;
 		lcm_newline();
 		lcm_print("start?");
-		while (GPIO_PORTF_DATA_R & 0x01);
+		while (GPIO_PORTF_DATA_R & 0x01);    //SW-2 shouled be ON (PUSHED) to start the microwave
 		break;
 
 	case 'B':
 		lcm_print_string("Beef weight?");
 		delay_ms(100);
 		m = keypad_clicked();
-		//Checking for valid kilos entered (1-9)
-		for (count = 1; count < 10; count++)
+		for (count = 1; count < 10; count++)    //Checking for valid kilos entered (1-9)
 		{
 			lcm_instruction(clear_display);
-			if ((m - '0') == count)
+			if ((m - '0') == count)       
 				break;
 			else if (count == 9)
 			{
-				lcm_print_string("Err");
+				lcm_print_string("Err");    //error message if user hasn't choosen a correct number for kilos
 				delay_s(1);
 				lcm_instruction(clear_display);
 				lcm_print_string("Beef weight?");
@@ -83,26 +82,25 @@ Begin:
 		lcm_instruction(clear_display);
 		lcm_print("start?");
 		i = m - '0';
-		z = 30 * i;
-		//lcm_print("Time remaining = z seconds");
+		z = 30 * i;   //it takes 0.5 minute(30 sec.) for whole kilo
+		//lcm_print("Time remaining = z in minutes & seconds");
 		minutes = z / 60;
 		seconds = z % 60;
-		while (GPIO_PORTF_DATA_R & 0x01);
+		while (GPIO_PORTF_DATA_R & 0x01);   //SW-2 shouled be ON (PUSHED) to start the microwave
 		break;
 
 	case 'C':
 		lcm_print_string("Chicken weight?");
 		delay_ms(100);
 		m = keypad_clicked();
-		//Checking for valid kilos entered (1-9)
-		for (count = 1; count < 10; count++)
+		for (count = 1; count < 10; count++)     //Checking for valid kilos entered (1-9)
 		{
 			lcm_instruction(clear_display);
 			if ((m - '0') == count)
 				break;
 			else if (count == 9)
 			{
-				lcm_print_string("Err");
+				lcm_print_string("Err");     //error message if user hasn't choosen a correct number for kilos
 				delay_s(1);
 				lcm_instruction(clear_display);
 				lcm_print_string("Chicken weight?");
@@ -116,10 +114,11 @@ Begin:
 		lcm_instruction(clear_display);
 		lcm_print("start?");
 		i = m - '0';
-		z = 12 * i;
+		z = 12 * i;         //it takes 0.2 minute(12 sec.) for whole kilo
+	        //lcm_print("Time remaining = z in minutes & seconds");
 		minutes = z / 60;
 		seconds = z % 60;
-		while (GPIO_PORTF_DATA_R & 0x01);
+		while (GPIO_PORTF_DATA_R & 0x01);     //SW-2 shouled be ON (PUSHED) to start the microwave
 		break;
 
 	case 'D':
@@ -129,15 +128,15 @@ Begin:
 		t[3] = '0';
 		t[4] = '0';
 		delay_ms(100);
-		while (1) {
+		while (1) { 
 			m = keypad_clicked();
-			if (m == '*')
+			if (m == '*')    //indicate to finish entering numbers
 				break;
-			else if (m == 'A' || m == 'B' || m == 'C' || m == 'D' || m == '#') {
+			else if (m == 'A' || m == 'B' || m == 'C' || m == 'D' || m == '#') {   //if user by false entered letter.. go back to choose a meal
 				lcm_instruction(clear_display);
 				lcm_print("Err");
 				delay_s(1);
-				lcm_instruction(return_home);
+				lcm_instruction(return_home);    //return for the main function to choose a meal
 			}
 			else {
 				t[0] = t[1];
@@ -155,12 +154,12 @@ Begin:
 		}
 		lcm_firstline();
 		lcm_print("start?");
-		while (GPIO_PORTF_DATA_R & 0x01);
+		while (GPIO_PORTF_DATA_R & 0x01);    //SW-2 shouled be ON (PUSHED) to start the microwave
 		break;
 	}
 }
 
-void start() {
+void start() {    //intiate the microwave system before start cooking
 	doorLockActive(1);
 	lcm_instruction(clear_display);
 	GPIO_PORTF_DATA_R |= 0x0E;
@@ -169,7 +168,7 @@ void start() {
 	GPIO_PORTE_DATA_R &= ~0x08;
 }
 
-GPIOD_Handler() {
+GPIOD_Handler() {    //handle door interrupts
 	lcm_movedisplay(20);
 	lcm_movecursor(0, 20);
 	lcm_print("CLOSE THE DOOR!!");
@@ -184,7 +183,7 @@ GPIOD_Handler() {
 	lcm_instruction(return_home);
 }
 
-GPIOF_Handler() {
+GPIOF_Handler() {     //handle leds interrupts
 	delay_ms(500);
 	while (1) {
 		GPIO_PORTF_DATA_R ^= 0x0E;
